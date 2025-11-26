7ddb807e5db0e1cf2dbf42893238d9b86842cfd2
Deepika Bhujel <dipikab062@gmail.com>
2025-11-26 12:11:41 +0545

    Task2

---
+++ Task2/Motorcycle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public class Motorcycle : Vehicle
+    {
+        public bool HasHelmet { get; set; }
+
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Console.WriteLine("Has Helmet: " + HasHelmet);
+        }
+
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car c = new Car { Brand = "Toyota", Speed = 160, Seats = 4 };
+            Motorcycle m = new Motorcycle { Brand = "Honda", Speed = 120, HasHelmet = true };
+
+            c.Start();
+            c.DisplayInfo();
+            c.Stop();
+
+            Console.WriteLine();
+
+            m.Start();
+            m.DisplayInfo();
+            m.Stop();
+
+            Console.WriteLine("");
+        }
+    }
+}
