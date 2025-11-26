703084d6400ef18338293063fedec95657b88d71
Deepika Bhujel <dipikab062@gmail.com>
2025-11-26 21:39:32 +0545

    Task3_1

---
+++ Task3_1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task3_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NepaliTeacher nt = new NepaliTeacher { Name = "Hari" };
+            EnglishTeacher et = new EnglishTeacher { Name = "John" };
+
+            nt.Teaching();
+            nt.SalaryInfo();
+
+            et.Teaching();
+            et.SalaryInfo();
+            Console.WriteLine("");
+        }
+    }
+}
