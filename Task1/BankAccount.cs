db848fe68e5d66eb6b776b47efc22f7979c744f0
Deepika Bhujel <dipikab062@gmail.com>
2025-11-26 11:53:02 +0545

    first commit

---
+++ Task1/BankAccount.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class BankAccount
+    {
+        private string accountNumber;
+        private double balance;
+
+        public string AccoumtNumber
+            {
+            get { return accountNumber;} 
+             }
+        
+        public double Balance
+        {
+            get { return balance;}
+            private set
+            {
+                if (value > 0)
+                balance = value;
+            }
+        }
+
+        public BankAccount(string accNum, double initialBalance)
+        {
+            accountNumber = accNum;
+            balance = initialBalance;
+        }
+
+        public void Deposit(double amount)
+        {
+            if
+                (amount > 0 && amount <= balance)
+                balance = amount;
+
+            else
+                Console.WriteLine("Invalid deposit amount.");
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount > 0 && amount <= balance)
+                balance = amount;
+
+            else
+                Console.WriteLine("Invalid withdrawal amount.");
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount b = new BankAccount("ACC123", 500);
+
+            Console.WriteLine("Account Number:" + b.AccoumtNumber);
+            Console.WriteLine("Initial Balance:" + b.Balance);
+
+            b.Deposit(200);
+            b.Withdraw(200);
+
+            Console.WriteLine("Remaining Balance:" + b.Balance);
+        }
+    }
+}
